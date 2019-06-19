9c653caac22662c0043e8362a00f673d3422d465
Ben <ben.mitchell@medecision.com>
2019-06-19 17:58:39 -0400

    starting display

--- mission-control-video/src/main/java/com/ten31f/mission/App.java
+++ mission-control-video/src/main/java/com/ten31f/mission/App.java
@@ -1,13 +1,10 @@
 package com.ten31f.mission;
 
-/**
- * Hello world!
- *
- */
-public class App 
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
+import com.ten31f.mission.display.Screen;
+
+public class App {
+	public static void main(String[] args) {
+		Screen screen = new Screen();
+		screen.start();
+	}
 }
